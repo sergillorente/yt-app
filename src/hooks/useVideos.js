3c05dafc211403c0b346e6c30ab399a7cccb740b
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2021-01-28 13:06:22 +0100

    ready for deploy

---
+++ src/hooks/useVideos.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+import youtube from '../apis/youtube';
+
+const KEY = 'AIzaSyBcWsmQXi9o1DzRbF_7-UXFWLOODR_zT8k';
+
+const useVideos = (defaultSearchTerm) => {
+    const [videos, setVideos] = useState([]);
+
+    useEffect(() => {
+        search(defaultSearchTerm);
+    }, [defaultSearchTerm]);
+
+    const search = async term => {
+        const response = await youtube.get("/search", {
+            params: {
+                q: term,
+                part: "snippet",
+                type: 'video',
+                maxResults: 5,
+                key: KEY
+            }
+        });
+
+        setVideos(response.data.items);
+    };
+
+    return [videos, search];
+};
+
+export default useVideos;
